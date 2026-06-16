6ca6ff50ee8d5a737d1a516830b2fbb4ac5f8962
飘雪缎绫 <unrealizablelingyi@outlook.com>
2026-06-17 02:17:30 +0800

    tokenauth

---
+++ src/main/java/gq/yozakura/auth/token/TokenAuthGuiHandler.java
@@ -0,0 +1,66 @@
+package gq.yozakura.auth.token;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiMultiplayer;
+import net.minecraft.util.Session;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
+
+public final class TokenAuthGuiHandler {
+    private static final int TOKEN_AUTH_BUTTON_ID = 7420;
+
+    private final Minecraft minecraft;
+    private final TokenAuthSessionManager sessionManager;
+
+    public TokenAuthGuiHandler() {
+        this.minecraft = Minecraft.getMinecraft();
+        this.sessionManager = new TokenAuthSessionManager(minecraft);
+    }
+
+    @SubscribeEvent
+    public void onInitGuiPost(GuiScreenEvent.InitGuiEvent.Post event) {
+        if (!(event.gui instanceof GuiMultiplayer) || hasButton(event.buttonList)) {
+            return;
+        }
+        event.buttonList.add(new GuiButton(TOKEN_AUTH_BUTTON_ID, 5, 5, 100, 20, "TokenAuth"));
+    }
+
+    @SubscribeEvent
+    public void onDrawScreenPost(GuiScreenEvent.DrawScreenEvent.Post event) {
+        if (!(event.gui instanceof GuiMultiplayer)) {
+            return;
+        }
+        Session session = sessionManager.getCurrentSession();
+        if (session == null) {
+            return;
+        }
+        String status = String.format("User: \u00a7a%s \u00a7rUUID: \u00a7a%s",
+                safe(session.getUsername()), safe(session.getPlayerID()));
+        minecraft.fontRendererObj.drawString(status, 115, 10, 0xFFFFFFFF);
+    }
+
+    @SubscribeEvent
+    public void onActionPerformedPre(GuiScreenEvent.ActionPerformedEvent.Pre event) {
+        if (!(event.gui instanceof GuiMultiplayer) || event.button == null
+                || event.button.id != TOKEN_AUTH_BUTTON_ID) {
+            return;
+        }
+        minecraft.displayGuiScreen(new TokenAuthSessionGui(event.gui, sessionManager));
+    }
+
+    private static boolean hasButton(List<GuiButton> buttons) {
+        for (GuiButton button : buttons) {
+            if (button.id == TOKEN_AUTH_BUTTON_ID) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String safe(String value) {
+        return value == null ? "" : value;
+    }
+}
--- src/main/java/gq/yozakura/core/Client.java
+++ src/main/java/gq/yozakura/core/Client.java
@@ -15,6 +15,7 @@ import gq.yozakura.command.WaterMark;
 import gq.yozakura.engine.font.FontLoaders;
 import gq.yozakura.bridge.YozakuraEventBridge;
 import gq.yozakura.auth.YozakuraAuthGate;
+import gq.yozakura.auth.token.TokenAuthGuiHandler;
 import gq.yozakura.ui.overlay.InjectionSuccessAnimation;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.client.C03PacketPlayer;
@@ -62,6 +63,7 @@ public class Client {
     public static boolean state = false;
     private static boolean shutdownHookRegistered;
     private boolean materialClickGuiResourcesWarmed;
+    private TokenAuthGuiHandler tokenAuthGuiHandler;
     public static Random rand=new Random();
     public final FileManager fileManager = new FileManager();
     public static ModuleManager moduleManager = new ModuleManager();
@@ -89,6 +91,8 @@ public class Client {
         }
         state = true;
         MinecraftForge.EVENT_BUS.register(this);
+        tokenAuthGuiHandler = new TokenAuthGuiHandler();
+        MinecraftForge.EVENT_BUS.register(tokenAuthGuiHandler);
         FMLCommonHandler.instance().bus().register(this);
         YozakuraEventBridge.init();
         instance = this;
@@ -186,6 +190,10 @@ public class Client {
             instance.fileManager.setAutoSaveSuspended(true);
             ModuleManager.disableAll(false);
             instance.fileManager.setAutoSaveSuspended(false);
+            if (instance.tokenAuthGuiHandler != null) {
+                MinecraftForge.EVENT_BUS.unregister(instance.tokenAuthGuiHandler);
+                instance.tokenAuthGuiHandler = null;
+            }
             MinecraftForge.EVENT_BUS.unregister(instance);
             FMLCommonHandler.instance().bus().unregister(instance);
             instance=null;
